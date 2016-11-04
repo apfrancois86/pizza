6576cb1605cfa8f79c76b76279a501062fe7e7f2
Alex Francois <apfrancois86@gmail.com>
2016-11-04 09:45:02 -0700

    add price Calculation prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 function Pizza(size) {
   this.size = size;
-  this.toppings= [];
+  this.toppings = [];
+}
+
+Pizza.prototype.priceCalc = function () {
+  var addedToppings = this.toppings.length * 1;
+  var totalCost = this.size + addedToppings;
+  return totalCost;
 }
